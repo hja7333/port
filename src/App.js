67e4699e41a7c70dbe46f142fe9cf2605492d4d6
Alan Hunt <hja7333@gmail.com>
2023-12-28 15:29:36 +0000

    conditionally render the text content of the button based on the darkMode state

--- src/App.js
+++ src/App.js
@@ -74,9 +74,9 @@ function App() {
                 <button
                   onClick={toggleDarkMode}
                   type="button"
-                  class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-xs px-3 py-1.5 text-center me-2 mb-2"
+                  className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-xs px-3 py-1.5 text-center me-2 mb-2"
                 >
-                  Dark Mode
+                  {darkMode ? "lightMode" : "darkMode"}
                 </button>
               </div>
             </div>
